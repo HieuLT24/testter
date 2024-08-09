5c3658b9f7a5ed2075b173d8517d450c38f99523
HieuLT24 <duchieuphung5@gmail.com>
2024-08-09 22:26:06 +0700

    luyen code js bai 6

---
+++ bai6/bt1.js
@@ -0,0 +1,33 @@
+// let timMax = (num1, num2) => {
+//     return num1>num2?num1:num2;
+// }
+
+// console.log(timMax(5,3));
+// console.log(timMax(-2,9));
+// console.log(timMax(0,3));
+// console.log(timMax(1111,3));
+
+
+// bt2
+
+// var fizzBuzz = (number) => {
+//         if(typeof(number) != "number")
+//             return "Ban hay nhap so!";
+    
+//         if(number % 3 === 0 && number % 5 === 0)
+//             return "FizzBUzz";
+//     if(number % 3 === 0)
+//         return "Fizz";
+//     if(number % 5 === 0)
+//         return "Buzz";
+//     if(number % 3 !== 0 || number % 5 !== 0)
+//         return number;
+// }
+
+// console.log(fizzBuzz(12));
+// console.log(fizzBuzz(25));
+// console.log(fizzBuzz(30));
+// console.log(fizzBuzz("one"));
+// console.log(fizzBuzz(7));
+
+// bt3
